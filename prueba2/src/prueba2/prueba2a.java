98c9050697c33cc4c2f348e762572a524a24757e
CHUKARO <lordchukaro@gmail.com>
2015-09-12 18:33:50 -0400

    modificado

--- prueba2/src/prueba2/prueba2a.java
+++ prueba2/src/prueba2/prueba2a.java
@@ -5,6 +5,7 @@ public class prueba2a {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo 2a");
+		System.out.println("Hola Mundo 2a-45");
 	}
 
 }
