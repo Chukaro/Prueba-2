15ff93879a197fa0c60593e411485c064c3cfb75
CHUKARO <lordchukaro@gmail.com>
2015-09-12 18:48:47 -0400

    System.out.println("Hola Mundo 2a-45");

--- prueba2/src/prueba2/prueba2a.java
+++ prueba2/src/prueba2/prueba2a.java
@@ -6,6 +6,7 @@ public class prueba2a {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo 2a");
 		System.out.println("Hola Mundo 2a-45");
+		System.out.println("Hola Mundo 2a-45");
 	}
 
 }
